93c651de1acfe6802164f00a678a057d08733cd3
Maxime Young <Maxime Young>
2018-11-18 21:37:05 -0500

    Ajout action afficher + Index

--- src/java/com/stageo/servlet/ControleurFrontal.java
+++ src/java/com/stageo/servlet/ControleurFrontal.java
@@ -1,6 +1,14 @@
 package com.stageo.servlet;
 
+//Action importer pour le controleur frontal
 import com.stageo.controleur.AbstractAction;
+import com.stageo.controleur.AfficherGestionDocumentsVueCoordinateurAction;
+import com.stageo.controleur.AfficherIncriptionAction;
+import com.stageo.controleur.AfficherListeEtudiantsVueEmployeurAction;
+import com.stageo.controleur.AfficherListeStagesVueEtudiantAction;
+import com.stageo.controleur.AfficherMessagerie;
+import com.stageo.controleur.AfficherGestionOffresStagesVueEmployeurAction;
+import com.stageo.controleur.AfficherProfilEtudiantAction;
 import com.stageo.controleur.DefaultAction;
 
 import java.io.IOException;
@@ -19,6 +27,27 @@ public class ControleurFrontal extends HttpServlet {
         if (actionAFaire == null) actionAFaire = "";
         
         switch (actionAFaire) {
+            case "afficherGestionDocumentsVueCoordinateur":
+                action = new AfficherGestionDocumentsVueCoordinateurAction();
+                break;
+            case "afficherGestionOffresStagesVueEmployeur":
+                action = new AfficherGestionOffresStagesVueEmployeurAction();
+                break;
+            case "afficherIncription":
+                action = new AfficherIncriptionAction();
+                break;
+            case "afficherListeEtudiantsVueEmployeur":
+                action = new AfficherListeEtudiantsVueEmployeurAction();
+                break;
+            case "afficherListeStagesVueEtudiant":
+                action = new AfficherListeStagesVueEtudiantAction();
+                break;
+            case "afficherProfilEtudiant":
+                action = new AfficherProfilEtudiantAction();
+                break;
+            case "afficherMessagerie":
+                action = new AfficherMessagerie();
+                break;
             default :
                 action = new DefaultAction();
         }
